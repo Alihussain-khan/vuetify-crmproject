91512141d1970aa9d9d3871cc9377f349aee0a56
Ali Hussain Khan <alihussainkhan24@gmail.com>
2024-02-15 12:48:06 +0500

    company form validation

--- src/components/AddCompany.vue
+++ src/components/AddCompany.vue
@@ -10,9 +10,10 @@
           <span class="text-h5">Add Company</span>
         </v-card-title>
         <v-container>
-          <v-form>
+          <v-form ref="form" v-model="valid">
             <v-card-text>
               <v-text-field
+                class="mb-2"
                 v-model="company.companyname"
                 :rules="companyrules"
                 clearable
@@ -21,6 +22,7 @@
               ></v-text-field>
 
               <v-text-field
+                class="mb-2"
                 label="User name"
                 v-model="company.username"
                 :rules="userrules"
@@ -28,6 +30,7 @@
               ></v-text-field>
 
               <v-text-field
+                class="mb-2"
                 label="Email"
                 v-model="company.useremail"
                 :rules="emailrules"
@@ -36,9 +39,10 @@
               ></v-text-field>
 
               <v-text-field
+                class="mb-2"
                 type="password"
                 v-model="company.password"
-                :rules="password"
+                :rules="passwordrules"
                 clearable
                 label="Password"
               ></v-text-field>
@@ -53,9 +57,10 @@
                 Close
               </v-btn>
               <v-btn
+                v-if="!valid && 'disable'"
                 color="blue-darken-1"
                 variant="text"
-                @click="dialog = false"
+                @click="printcompany"
               >
                 Save
               </v-btn>
@@ -77,7 +82,66 @@ export default {
         useremail: null,
         password: null,
       },
+      valid: false,
+      companyrules: [
+        (value) => {
+          if (value) return true;
+
+          return "* Field Cannot Be Empty.";
+        },
+        (value) => {
+          if (value.length >= 3) return true;
+
+          return "* Company Name Cannot be less than 3 characters";
+        },
+      ],
+      userrules: [
+        (value) => {
+          if (value) return true;
+
+          return "* Field Cannot Be Empty.";
+        },
+        (value) => {
+          if (value.length >= 3) return true;
+
+          return "* Name Cannot be less than 3 characters";
+        },
+      ],
+      emailrules: [
+        (value) => {
+          if (value) return true;
+
+          return "* Field Cannot Be Empty.";
+        },
+        (value) => {
+          if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+            return true;
+          }
+
+          return "* Enter Valid email";
+        },
+      ],
+      passwordrules: [
+        (value) => {
+          if (value) return true;
+
+          return "* Field Cannot Be Empty.";
+        },
+        (value) => {
+          if (value.length >= 8) return true;
+
+          return "* Password Cannot be less than 8";
+        },
+      ],
     };
+  },
+  methods: {
+    async printcompany() {
+      if (this.$refs.form.validate() && this.valid) {
+        console.log(this.company);
+        alert("added");
+      }
+    },
   },
 };
 </script>
